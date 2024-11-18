4c94afe43289316327c462f68d4674ea879cfda2
larissamendes122 <161464972+larissamendes122@users.noreply.github.com>
2024-11-18 11:47:45 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,49 @@
+criaCartao(
+    'Batom',
+    'O que é batom?',
+    'O batom é um cosmético, usado para colorir os lábios'
+)
+
+criaCartao(
+    'Rímel',
+    'O que é rímel?',
+    'É um produto usado para colorir e alongar os cílios'
+)
+
+
+criaCartao(
+    'base',
+    'O que é base?',
+    'Um produto usado para encobrir imperfeiçoẽs no rosto'
+)
+
+criaCartao(
+    'Corretivo',
+    'O que é corretivo?',
+    'Produto usado para cobrir olheiras'
+)
+
+criaCartao(
+    'Pó',
+    'O que é Pó?',
+    'O Pó é usado para retirar a oleosidade do rosto'
+)
+
+criaCartao(
+    'blush',
+    'O que é blush?',
+    'blush é um pigmento usado na bochecha'
+)
+
+
+criaCartao(
+    'Contorno',
+    'O que é contorno?',
+    'Produto usado para contonar o rosto,dando a impressão de mais fino'
+)
+
+criaCartao(
+    'fixador',
+    'O que é fixador?',
+    'Produto usado para fixar a maquiagem'
+)
